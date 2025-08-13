2ffaf1dd0c18f5691cfa6c19cec0011ba9e9341c
이상우 <dogcenta@gmail.com>
2025-08-13 13:07:37 +0900

    [Silver IV] Title: 제로, Time: 76 ms, Memory: 33192 KB -BaekjoonHub

---
+++ 백준/Silver/10773. 제로/제로.py
@@ -0,0 +1,14 @@
+import sys
+
+K = int(sys.stdin.readline())
+
+stack = []
+
+for _ in range(K):
+    N = int(sys.stdin.readline().rstrip())
+    if N == 0:
+        stack.pop()
+    else :
+        stack.append(N)
+        
+print(sum(stack))
